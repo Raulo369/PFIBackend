5ae8f9dec68ef3d7b29ffdb459965335a7dbf1bb
Raúl <92900684+Raulo369@users.noreply.github.com>
2022-08-25 23:02:49 -0300

    Initial commit

---
+++ src/main/java/com/PFI_RaulBarilla/PFI/PfiApplication.java
@@ -0,0 +1,13 @@
+package com.PFI_RaulBarilla.PFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PFI_RaulBarilla/PFI/PfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PFI_RaulBarilla.PFI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
