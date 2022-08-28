4adf81f0493b9c08bfd7efbb68046fde4bc8cb52
Raúl <92900684+Raulo369@users.noreply.github.com>
2022-08-27 23:27:33 -0300

    segundo intento con java anterior version estable

--- src/main/java/com/PFI_RaulBarilla/PFI/repo/EmployeeRepo.java
+++ src/main/java/com/PFI_RaulBarilla/PFI/repo/EmployeeRepo.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
     void deleteEmployeeById(Long id);
 
-   Optional<Employee> employee findEmployeeById(Long id);
+   Optional employee findEmployeeById(Long id);
 }
